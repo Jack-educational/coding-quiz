6da94f0e66ce4343d8586c43d6172ebd15d6d500
Jack Turner <34825609+neverekt@users.noreply.github.com>
2023-03-14 20:07:30 +0000

    Added 5 more questions to bring total number to 10

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -48,5 +48,56 @@ const quizData = [
       "An HTML tag"
     ],
     answer: 1
+  },
+  {
+    question: "What is the difference between == and === in JavaScript?",
+    choices: [
+      "== compares values after converting the types if necessary, while === only compares values of the same type",
+      "== compares values of the same type, while === only compares values after converting the types if necessary",
+      "== compares values without converting the types, while === compares values after converting the types if necessary",
+      "== compares values after converting the types if necessary, while === compares values without converting the types"
+  
+    ],
+    answer: 3
+  },
+  {
+    question: "What is the purpose of the this keyword in JavaScript?",
+    choices: [
+      "To refer to the current object or context",
+      "To declare a new variable",
+      "To define a new function",
+      "To create a new object"
+    ],
+    answer: 0
+  },
+  {
+    question: "What is the DOM in JavaScript?",
+    choices: [
+      "The Document Object Manager, which manages the creation and deletion of HTML documents",
+      "The Document Object Method, which provides methods for manipulating HTML documents",
+      "The Document Object Model, which represents the structure of an HTML document as a tree of objects",
+      "The Document Object Machine, which executes JavaScript code in the browser"
+    ],
+    answer: 2
+  },
+  {
+    question: "What is AJAX in JavaScript?",
+    choices: [
+      "A JavaScript library for building user interfaces",
+      "Asynchronous JavaScript and XML",
+      "A method of organizing CSS styles",
+      "A programming language that runs in the browser"
+    ],
+    answer: 1
+  },
+  {
+    question: "What is event bubbling in JavaScript?",
+    choices: [
+      "The propagation of an event from one element to another element at the same level in the DOM hierarchy",
+      "The propagation of an event from one browser window to another browser window",
+      "The propagation of an event from the innermost element to the outermost element in the DOM hierarchy",
+      "The propagation of an event from the outermost element to the innermost element in the DOM hierarchy"
+    ],
+    answer: 2
   }
 ];
