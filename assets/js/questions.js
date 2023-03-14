a0a7eeeed520c77b45ccd4e36cc62631a31f8649
Jack Turner <34825609+neverekt@users.noreply.github.com>
2023-03-14 19:52:06 +0000

    added 5 questions in json array for coding quiz along with answers

---
+++ assets/js/questions.js
@@ -0,0 +1,52 @@
+// store of all quiz questions and answers as an array
+const quizData = [
+  {
+    question: "What is JavaScript?",
+    choices: [
+      "A type of coffee that programmers drink",
+      "A tool for creating responsive websites",
+      "A scripting or programming language that allows you to implement complex features on web pages",
+      "A method of organizing CSS styles"
+    ],
+    answer: 2
+  },
+  {
+    question: "What is a variable?",
+    choices: [
+      "A container for storing data values",
+      "A type of function",
+      "An HTML tag",
+      "A CSS property"
+    ], answer: 0
+  },
+  {
+    question: "What is an array?",
+    choices: [
+      "A type of loop",
+      "A type of function",
+      "An HTML tag",
+      "A special variable that can hold more than one value",
+    ],
+    answer: 3
+  },
+  {
+    question: "What is a function?",
+    choices: [
+      "A type of variable",
+      "A block of code designed to perform a particular task",
+      "An HTML tag",
+      "A CSS property"
+    ],
+    answer: 1
+  },
+  {
+    question: "What is an if statement?",
+    choices: [
+      "A type of loop",
+      "A statement that checks if a condition is true and performs a task if it is",
+      "A type of function",
+      "An HTML tag"
+    ],
+    answer: 1
+  }
+];
